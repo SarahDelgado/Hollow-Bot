e92bbd3d77ec97233cd7de760345e9642e911033
Sarah Delgado Martin <sarahdelgado36@gmail.com>
2025-04-15 22:48:39 +0200

    Update renombrarImagenesYTxt.py

--- Procesamiento-img/renombrarImagenesYTxt.py
+++ Procesamiento-img/renombrarImagenesYTxt.py
@@ -2,12 +2,15 @@ import os
 
 # Código para renombrar imágenes y sus txt correspondientes
 
-# Directorios donde están los archivos
-directorio_png = "dataset/json"
-directorio_txt = "txt"
-
-# Función para renombrar archivos en un directorio
 def renombrar_archivos(directorio, extension):
+    """
+    Renombra todos los archivos en el directorio especificado que tengan la extensión dada,
+    agregando el sufijo '_v2' al nombre base de cada archivo.
+
+    Args:
+        directorio (str): Ruta del directorio que contiene los archivos a renombrar.
+        extension (str): Extensión de los archivos que se desean renombrar (por ejemplo, ".json" o ".txt").
+    """
     # Itera sobre todos los archivos de un directorio
     for archivo in os.listdir(directorio):
         # Comprueba que sea del tipo establecido por parámetro, es decir, que tenga al extensión deseada
@@ -19,6 +22,11 @@ def renombrar_archivos(directorio, extension):
             os.rename(ruta_actual, ruta_nueva) # Renombra el archivo
             print(f"Renombrado: {archivo} → {nuevo_nombre}")
 
+
+# Directorios donde están los archivos
+directorio_png = "dataset/json"
+directorio_txt = "txt"
+
 # Renombrar archivos en ambos directorios
 renombrar_archivos(directorio_png, ".json")
-# renombrar_archivos(directorio_txt, ".txt")
+renombrar_archivos(directorio_txt, ".txt")
