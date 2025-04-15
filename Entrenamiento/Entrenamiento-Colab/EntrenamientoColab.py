cfc8c7f01a087093d9b9dd263a67514aa1e0830b
Sarah Delgado Martin <sarahdelgado36@gmail.com>
2025-04-15 19:46:03 +0200

    Delete Entrenamiento/Entrenamiento-Colab/EntrenamientoColab.py

--- Entrenamiento/Entrenamiento-Colab/EntrenamientoColab.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Cargar modelo YOLOv8
-model = YOLO("yolov8n.pt")
-
-# Entrenar modelo usando data_character.yaml
-model.train(data="dataset/data_character.yaml", epochs=50, imgsz=640)
